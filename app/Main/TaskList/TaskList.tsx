b959fc20187c27111e3f72aef0110857461ac075
laricko <ttttmmm6432@gmail.com>
2023-07-03 21:44:52 -0700

    Add create popup

--- app/Main/TaskList/TaskList.tsx
+++ app/Main/TaskList/TaskList.tsx
@@ -1,5 +1,6 @@
 import { Task } from "../Task";
 import style from "./TaskList.module.css";
+import { TaskEdit } from "../TaskEdit";
 
 import { useState } from "react";
 
@@ -14,20 +15,36 @@ export default function TaskList({ tasks }) {
     setActiveCard(null);
   };
 
-  return (
-    <ul className={style.taskList}>
-      {tasks.map((task, index) => (
-        <li className={style.taskItem} key={index}>
-          {
-            <Task
-              task={task}
-              isActive={activeCard === task.id}
-              handleCardClick={handleCardClick}
-              handleFormClose={handleFormClose}
-            />
-          }
-        </li>
-      ))}
-    </ul>
+  const [showCreateTask, setShowCreateTask] = useState(false);
+
+  let taskList = (
+    <div className={style.container}>
+      <button
+        className={style.buttonPlus}
+        onClick={() => {
+          setShowCreateTask(!showCreateTask);
+        }}
+      >
+        +
+      </button>
+      <ul className={style.taskList}>
+        {tasks.map((task, index) => (
+          <li className={style.taskItem} key={index}>
+            {
+              <Task
+                task={task}
+                isActive={activeCard === task.id}
+                handleCardClick={handleCardClick}
+                handleFormClose={handleFormClose}
+              />
+            }
+          </li>
+        ))}
+      </ul>
+    </div>
   );
+
+  let taskCreate = <TaskEdit task={{}} setShowTaskCard={setShowCreateTask} />;
+
+  return showCreateTask ? taskCreate : taskList;
 }
